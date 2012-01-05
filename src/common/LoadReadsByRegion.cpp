ad89b869ea8ced917bb0aecbbddfc6c022663895
songqiang <qiang.song@usc.edu>
2012-01-05 01:42:40 +0000

    Allow reads input file to be either .bam format or .bed format

--- src/common/LoadReadsByRegion.cpp
+++ src/common/LoadReadsByRegion.cpp
@@ -24,6 +24,8 @@
 
 #include "smithlab_utils.hpp"
 
+#include "BAMFile.hpp"
+
 #include <iomanip>
 #include <fstream>
 
@@ -39,8 +41,8 @@ using std::max;
  * This function takes the names of three files (a reads file, a
  * chromosome file, and a dead zones file [possibly empty])
  */
-void
-LoadReadsByRegion(const bool VERBOSE,
+static void
+LoadReadsByRegionBED(const bool VERBOSE,
 		  const std::string &chroms_file, 
 		  const std::string &reads_file, 
 		  const std::string &deads_file,
@@ -122,8 +124,8 @@ LoadReadsByRegion(const bool VERBOSE,
  * This function takes the names of four files (reads file a, reads
  * file b, a chromosome file, and a dead zones file [possibly empty])
  */
-void
-LoadReadsByRegion(const bool VERBOSE,
+static void
+LoadReadsByRegionBED(const bool VERBOSE,
 		  const std::string &chroms_file, 
 		  const std::string &reads_file_a, 
 		  const std::string &reads_file_b, 
@@ -229,3 +231,249 @@ LoadReadsByRegion(const bool VERBOSE,
     i = i > 0 ? i - 1 : 0;
   }
 }
+
+/*************************************************
+ * This function takes the names of three files (a reads file, a
+ * chromosome file, and a dead zones file [possibly empty])
+ */
+static void
+LoadReadsByRegionBAM(const bool VERBOSE,
+		  const std::string &chroms_file, 
+		  const std::string &reads_file, 
+		  const std::string &deads_file,
+		  const size_t bin_size, 
+          std::vector<SimpleGenomicRegion> &bin_boundaries,
+          std::vector<double> &read_bins,
+          std::vector<double> &nondead_scales,
+          std::vector<size_t> &reset_points,
+          const bool REMOVE_JACKPOT) 
+{
+  // get the chroms
+  if (VERBOSE)
+    cout << "[LOADING_DATA] chromosomes" << endl;
+  vector<SimpleGenomicRegion> chroms;
+  ReadBEDFile(chroms_file, chroms);
+  if (!check_sorted(chroms, true))
+    SortGenomicRegion::sort_regions_collapse(chroms);
+
+  // Create bins
+  reset_points.push_back(0);
+  for (size_t i = 0; i < chroms.size(); ++i) {
+    const string chrom(chroms[i].get_chrom());
+    for (size_t j = 0; j < chroms[i].get_width(); j += bin_size) 
+      bin_boundaries.push_back(SimpleGenomicRegion(chrom, j, j + bin_size));
+    if (bin_boundaries.size() > reset_points.back())
+        reset_points.push_back(bin_boundaries.size());
+  }
+
+  // Load the reads, tabulating counts in bins
+  if (VERBOSE)
+      cout << "[LOADING_DATA] reads"  << endl;
+  read_bins.resize(bin_boundaries.size(), 0);
+  BAMFile in(reads_file);
+  size_t i = 0;
+  GenomicRegion prev_gr, gr;
+  while ((in >> gr).good()) {
+    if (REMOVE_JACKPOT
+        && prev_gr.get_start() == gr.get_start()
+        && prev_gr.get_strand() == gr.get_strand()
+        && prev_gr.get_chrom() == gr.get_chrom())
+        continue;
+    if (gr < prev_gr) {
+      cerr << "ERROR: reads not sorted in " << reads_file << endl;
+      exit(-1);
+    }
+    prev_gr = gr;
+    if (gr.pos_strand()) gr.set_end(gr.get_start() + 1);
+    else gr.set_start(gr.get_end() - 1);
+
+    while (i < bin_boundaries.size() && bin_boundaries[i] < gr) ++i;
+    if (i >= bin_boundaries.size() || !bin_boundaries[i].contains(gr))
+      continue;
+    ++read_bins[i];
+  }
+
+  // load the dead zones
+  if (VERBOSE)
+      cerr << "[LOADING_DATA] deadzones"  << endl;
+  nondead_scales.resize(bin_boundaries.size(), 1.0);
+  std::ifstream dead_in(deads_file.c_str());
+  i = 0;
+  string line;
+  while (getline(dead_in, line)) {
+    SimpleGenomicRegion gr(line);
+    while (i < bin_boundaries.size() && !bin_boundaries[i].overlaps(gr)) ++i;
+    while (i < bin_boundaries.size() && bin_boundaries[i].overlaps(gr)) {
+      const double dead = min(bin_boundaries[i].get_end(), gr.get_end())
+          - max(bin_boundaries[i].get_start(), gr.get_start());
+      nondead_scales[i] -= dead / bin_size;
+      ++i;
+    }
+    i = i > 0 ? i - 1 : 0;
+  }
+}
+
+/*************************************************
+ * This function takes the names of four files (reads file a, reads
+ * file b, a chromosome file, and a dead zones file [possibly empty])
+ */
+static void
+LoadReadsByRegionBAM(const bool VERBOSE,
+		  const std::string &chroms_file, 
+		  const std::string &reads_file_a, 
+		  const std::string &reads_file_b, 
+		  const std::string &deads_file,
+		  const size_t bin_size, 
+          std::vector<SimpleGenomicRegion> &bin_boundaries,
+          std::vector<double> &read_bins_a,
+          std::vector<double> &read_bins_b,
+		  std::vector<double> &nondead_scales,
+          std::vector<size_t> &reset_points,
+          const bool REMOVE_JACKPOT)
+{
+  // get the chroms
+  if (VERBOSE)
+    cout << "[LOADING_DATA] chromosomes" << endl;
+  vector<SimpleGenomicRegion> chroms;
+  ReadBEDFile(chroms_file, chroms);
+  if (!check_sorted(chroms, true))
+    SortGenomicRegion::sort_regions_collapse(chroms);
+
+  // Create bins
+  reset_points.push_back(0);
+  for (size_t i = 0; i < chroms.size(); ++i) {
+    const string chrom(chroms[i].get_chrom());
+    for (size_t j = 0; j < chroms[i].get_width(); j += bin_size) 
+      bin_boundaries.push_back(SimpleGenomicRegion(chrom, j, j + bin_size));
+    if (bin_boundaries.size() > reset_points.back())
+        reset_points.push_back(bin_boundaries.size());
+  }
+
+  // Load the reads, tabulating counts in bins
+  if (VERBOSE)
+      cout << "[LOADING_DATA] reads"  << endl;
+  read_bins_a.resize(bin_boundaries.size(), 0);
+  BAMFile in(reads_file_a);
+  size_t i = 0;
+  GenomicRegion prev_gr, gr;
+  while ((in >> gr).good()) {
+    if (REMOVE_JACKPOT
+        && prev_gr.get_start() == gr.get_start()
+        && prev_gr.get_strand() == gr.get_strand()
+        && prev_gr.get_chrom() == gr.get_chrom())
+        continue;
+    if (gr < prev_gr) {
+      cerr << "ERROR: reads not sorted in " << reads_file_a << endl;
+      exit(-1);
+    }
+    prev_gr = gr;
+    if (gr.pos_strand()) gr.set_end(gr.get_start() + 1);
+    else gr.set_start(gr.get_end() - 1);
+
+    while (i < bin_boundaries.size() && bin_boundaries[i] < gr) ++i;
+    if (i >= bin_boundaries.size() || !bin_boundaries[i].contains(gr))
+      continue;
+    ++read_bins_a[i];
+  }
+
+  // Load the reads, tabulating counts in bins
+  read_bins_b.resize(bin_boundaries.size(), 0);
+  in.close();
+  in.open(reads_file_b);
+  i = 0;
+  prev_gr = GenomicRegion();
+  while ((in >> gr).good()) {
+    if (REMOVE_JACKPOT
+        && prev_gr.get_start() == gr.get_start()
+        && prev_gr.get_strand() == gr.get_strand()
+        && prev_gr.get_chrom() == gr.get_chrom())
+        continue;
+    if (gr < prev_gr) {
+      cerr << "ERROR: reads not sorted in " << reads_file_b << endl;
+      exit(-1);
+    }
+    prev_gr = gr;
+    if (gr.pos_strand()) gr.set_end(gr.get_start() + 1);
+    else gr.set_start(gr.get_end() - 1);
+
+    while (i < bin_boundaries.size() && bin_boundaries[i] < gr) ++i;
+    if (i >= bin_boundaries.size() || !bin_boundaries[i].contains(gr))
+      continue;
+    ++read_bins_b[i];
+  }
+
+  // load the dead zones
+  if (VERBOSE)
+      cout << "[LOADING_DATA] deadzones"  << endl;
+  nondead_scales.resize(bin_boundaries.size(), 1.0);
+  std::ifstream dead_in(deads_file.c_str());
+  i = 0;
+  string line;
+  while (getline(dead_in, line)) {
+    SimpleGenomicRegion gr(line);
+    while (i < bin_boundaries.size() && !bin_boundaries[i].overlaps(gr)) ++i;
+    while (i < bin_boundaries.size() && bin_boundaries[i].overlaps(gr)) {
+      const double dead = min(bin_boundaries[i].get_end(), gr.get_end())
+          - max(bin_boundaries[i].get_start(), gr.get_start());
+      nondead_scales[i] -= dead / bin_size;
+      ++i;
+    }
+    i = i > 0 ? i - 1 : 0;
+  }
+}
+
+
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+void
+LoadReadsByRegion(const bool VERBOSE,
+		  const std::string &chroms_file, 
+		  const std::string &reads_file, 
+		  const std::string &deads_file,
+		  const size_t bin_size, 
+          std::vector<SimpleGenomicRegion> &bin_boundaries,
+          std::vector<double> &read_bins,
+          std::vector<double> &nondead_scales,
+          std::vector<size_t> &reset_points,
+          const bool REMOVE_JACKPOT)
+{
+    const string ext_name = reads_file.substr(reads_file.find_last_of('.'));
+    if (ext_name == ".bed" || ext_name == ".BED")
+        LoadReadsByRegionBED(VERBOSE, chroms_file, reads_file, deads_file,
+                             bin_size, bin_boundaries, read_bins,
+                             nondead_scales, reset_points, REMOVE_JACKPOT);
+    else
+        LoadReadsByRegionBAM(VERBOSE, chroms_file, reads_file, deads_file,
+                             bin_size, bin_boundaries, read_bins,
+                             nondead_scales, reset_points, REMOVE_JACKPOT);
+}
+
+
+void
+LoadReadsByRegion(const bool VERBOSE,
+		  const std::string &chroms_file, 
+		  const std::string &reads_file_a, 
+		  const std::string &reads_file_b, 
+		  const std::string &deads_file,
+		  const size_t bin_size, 
+          std::vector<SimpleGenomicRegion> &bin_boundaries,
+          std::vector<double> &read_bins_a,
+          std::vector<double> &read_bins_b,
+          std::vector<double> &nondead_scales,
+          std::vector<size_t> &reset_points,
+          const bool REMOVE_JACKPOT)
+{
+    const string ext_name = reads_file_a.substr(reads_file_a.find_last_of('.'));
+    if (ext_name == ".bed" || ext_name == ".BED")
+        LoadReadsByRegionBED(VERBOSE, chroms_file, reads_file_a, reads_file_b,
+                             deads_file, bin_size, bin_boundaries,
+                             read_bins_a, read_bins_b, nondead_scales,
+                             reset_points, REMOVE_JACKPOT);
+    else
+        LoadReadsByRegionBAM(VERBOSE, chroms_file, reads_file_a, reads_file_b,
+                             deads_file, bin_size, bin_boundaries,
+                             read_bins_a, read_bins_b, nondead_scales,
+                             reset_points, REMOVE_JACKPOT);
+}
+
+
