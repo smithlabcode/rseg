8388f4653bbb928d3e34e645716e505d55ded1a6
Andrew D Smith <andrewds@usc.edu>
2025-11-03 19:19:32 -0800

    src/common/ModelParams.cpp: modernizing

--- src/common/ModelParams.cpp
+++ src/common/ModelParams.cpp
@@ -19,109 +19,86 @@
  * 02110-1301 USA
  */
 
-#include <vector>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iterator>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
-#include "smithlab_utils.hpp"
+template <class Distro_Type>
+void
+read_param_file(const std::string &infile, const size_t n,
+                std::vector<double> &start_trans,
+                std::vector<std::vector<double>> &trans,
+                std::vector<double> &end_trans,
+                std::vector<Distro_Type> &distros) {
 
-using std::vector;
-using std::string;
-using std::endl;
-using std::cerr;
-using std::getline;
+  std::ifstream in(infile);
+  if (!in)
+    throw std::runtime_error("failed to open: " + infile);
 
+  std::size_t n_from_file{};
+  in >> n_from_file;
+  if (n_from_file != n)
+    throw std::runtime_error("Mismatching number of states");
 
-static void
-convert_to_stringstream(const string &infile, std::stringstream &ss)
-{
-    std::ifstream in(infile.c_str());
-    
-    while (!in.eof())
-    {
-        string str;
-        getline(in, str);
-        const size_t comment_start = str.find("#");
-        if (comment_start != string::npos)
-            str.erase(comment_start);
-        str = smithlab::strip(str);
-        if (!str.empty())
-            ss << str << endl;
-    }
+  std::string tmp_str;
+  std::getline(in, tmp_str);
+
+  distros.clear();
+  for (size_t i = 0; i < n; ++i) {
+    std::string tmp_str;
+    std::getline(in, tmp_str);
+    distros.push_back(Distro_Type(tmp_str));
+  }
+
+  start_trans.resize(n);
+  for (size_t i = 0; i < n; ++i)
+    in >> start_trans[i];
+
+  trans.resize(n, std::vector<double>(n));
+  for (size_t i = 0; i < n; ++i)
+    for (size_t j = 0; j < n; ++j)
+      in >> trans[i][j];
+
+  end_trans.resize(n);
+  for (size_t i = 0; i < n; ++i)
+    in >> end_trans[i];
 }
 
+template <class Distro_Type>
+void
+write_param_file(const std::string &outfile, const size_t &n,
+                 const std::vector<double> &start_trans,
+                 const std::vector<std::vector<double>> &trans,
+                 const std::vector<double> &end_trans,
+                 const std::vector<Distro_Type> &distros) {
+  std::ofstream out(outfile);
+  if (!out)
+    throw std::runtime_error("failed to open file: " + outfile);
 
-template<class Distro_Type> void
-read_param_file(const string &infile, size_t &n,
-                vector<double> &start_trans,
-                vector<vector<double> > &trans,
-                vector<double> &end_trans,
-                vector<Distro_Type> &distros)
-{
-    std::stringstream ss(std::stringstream::in | std::stringstream::out);
-    convert_to_stringstream(infile, ss);
-    
-    ss >> n; 
-    string tmp_str;
-    getline(ss, tmp_str);
+  out << "# number of states\n";
+  out << n << '\n';
 
-    distros.clear();
-    for (size_t i = 0; i < n; ++i)
-    {
-        string tmp_str;
-        getline(ss, tmp_str);
-        distros.push_back(Distro_Type(tmp_str));
-    }
-    
-    start_trans.resize(n);
-    for (size_t i = 0; i < n; ++i)
-        ss >> start_trans[i];
-    
-    trans.resize(n, vector<double>(n));
-    for (size_t i = 0; i < n; ++i)
-        for (size_t j = 0; j < n; ++j)
-            ss >> trans[i][j];
+  out << "\n# emmission distributions\n";
+  std::copy(std::cbegin(distros), std::cend(distros),
+            std::ostream_iterator<Distro_Type>(out, "\n"));
 
-    end_trans.resize(n);
-    for (size_t i = 0; i < n; ++i)
-        ss >> end_trans[i];
+  out << "\n# start to state transition probabilities\n";
+  std::copy(std::cbegin(start_trans), std::cend(start_trans),
+            std::ostream_iterator<double>(out, "\n"));
+
+  out << "\n# state transition probabilities\n";
+  for (size_t i = 0; i < n; ++i) {
+    std::copy(std::cbegin(trans[i]), std::cend(trans[i]),
+              std::ostream_iterator<double>(out, "\t"));
+    out << '\n';
+  }
+
+  out << "\n# states to end transition probabilities\n";
+  std::copy(std::cbegin(end_trans), std::cend(end_trans),
+            std::ostream_iterator<double>(out, "\n"));
 }
-
-template <class Distro_Type> void
-write_param_file(const string &outfile, 
-                 const size_t &n,
-                 const vector<double> &start_trans,
-                 const vector<vector<double> > &trans,
-                 const vector<double> &end_trans,
-                 const vector<Distro_Type> &distros)
-{
-    std::ofstream out(outfile.c_str());
-    
-    out << "# number of states" << endl;
-    out << n << endl;
-    
-    out << "\n# emmission distributions" << endl;
-    std::copy(distros.begin(), distros.end(),
-              std::ostream_iterator<Distro_Type>(out, "\n"));
-    
-    out << "\n# start to state transition probabilities" << endl;
-    std::copy(start_trans.begin(), start_trans.end(),
-              std::ostream_iterator<double>(out, "\n"));
-    
-    out << "\n# state transition probabilities" << endl;
-    for (size_t i = 0; i < n; ++i)
-    {
-        copy(trans[i].begin(), trans[i].end(),
-             std::ostream_iterator<double>(out, "\t"));
-        out << endl;
-    }
-    
-    out << "\n# states to end transition probabilities" << endl;
-    std::copy(end_trans.begin(), end_trans.end(),
-              std::ostream_iterator<double>(out, "\n"));
-}
-
