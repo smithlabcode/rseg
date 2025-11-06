01bdbffae8ab1c7ff91ff179c89160fb6017dbe1
Andrew D Smith <andrewds@usc.edu>
2025-11-06 10:15:31 -0800

    src/rseg/deadzones.cpp: formatting

--- src/rseg/deadzones.cpp
+++ src/rseg/deadzones.cpp
@@ -1,21 +1,22 @@
-/*    deadzones: A program for identifying genomic deadzones
- *    Copyright (C) 2009 University of Southern California and
- *                       Andrew D. Smith
+/* deadzones: A program for identifying genomic deadzones
  *
- *    Authors: Andrew D. Smith
+ * Copyright (C) 2009 University of Southern California and
+ *                    Andrew D. Smith
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * Authors: Andrew D. Smith
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "GenomicRegion.hpp"
